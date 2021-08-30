05b80fce0f10144e88389d951328df4b14a89d8a
Nik-hil-1 <wwsnikhil@gmail.com>
2021-08-30 12:32:24 +0530

    Re-modified

--- src/com/gl/itsupportadmin/service/Service.java
+++ src/com/gl/itsupportadmin/service/Service.java
@@ -5,17 +5,17 @@ import com.gl.itsupportadmin.pojo.Employee;
 
 public class Service {
 	
-    public StringBuilder generatePassword() {
+    public String generatePassword() {
 		
 		String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"+"abcdefghijklmnopqrstuvwxyz"+"0123456789"+"!@#$%^&*()-=_+?/><";
 		
-		StringBuilder password = new StringBuilder();
+		String password = "";
 		Random random = new Random();
 		char[] buildPassword = new char[8];
 		
 		for(int i = 0; i<8; i++) {
 			buildPassword[i] = letters.charAt(random.nextInt(letters.length()));
-			password.append(buildPassword[i]);
+			password += buildPassword[i];
 		}
 		
 		return password;
@@ -24,12 +24,12 @@ public class Service {
 	public String generateEmail(Employee emp,  String department) {
 		
 		String fullName = emp.getFirstName()+emp.getLastName();
-		StringBuilder name = new StringBuilder();
+		String name = "";
 		char[] lowerCase = new char[fullName.length()];
 		
 		for(int i=0; i<fullName.length(); i++) {
 			lowerCase[i] = Character.toLowerCase(fullName.charAt(i));
-			name.append(lowerCase[i]);
+			name += lowerCase[i];
 		}
 		
         return name+"."+department+"@greatlearning.com";
